c44a7f9226795cedc6cda02265f6f9483a22304c
Uldana Zhaksybay <uldana.zhaksibay2000@gmail.com>
2021-03-06 17:35:25 +0600

    add readme'

---
+++ gitp/gitp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
