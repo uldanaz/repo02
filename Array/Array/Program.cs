3e8ec36f1f3a33ef9f39e5d491c880dbb3b2e604
Uldana Zhaksybay <uldana.zhaksibay2000@gmail.com>
2021-03-07 01:27:46 +0600

    Home Work - Array

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -6,7 +6,7 @@ namespace Array
     {
         static void Main()
         {
-            int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
+            int[] array = new int[] {0,1,2,3,4,5, 6,7,8,9};
             foreach (int i in array)
                 Console.WriteLine(i);
               
